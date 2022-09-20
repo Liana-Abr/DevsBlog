7bc8cabef8cc9ed3ea9836fdf2ce9f37debae9a6
Liana <AbramyanLV20@st.ithub.ru>
2022-09-20 21:17:28 +0300

    Примерная форма создания поста

--- src/pages/CreatePost/index.jsx
+++ src/pages/CreatePost/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-
+import "./style.css"
 export default () => {
   return (
     <div>
       <h1>Тут можно создать пост</h1>
+        <form className="form-createpost" action="" >
+            <input type="text" name="name" placeholder="Название"  />
+            <br />
+            <input type="text" name="description" placeholder="Описание"  />
+            <br />
+            <input type="url" placeholder="image link" name="img_link" />
+            <br />
+            <input type="text" name="author" placeholder="Автор"  />
+            <br />
+            <input type="text" name="createdAt" placeholder="2022-09-05"  />
+            <br />
+            <button type="submit">Создать пост</button>
+        </form>
     </div>
   );
 };
